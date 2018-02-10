93ef61a3fb3f6ec9f3204e2bdf089fedef19be01
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-10 16:13:29 +0100

    Added new views to the application.

--- ExploreTours/Models/Tour.cs
+++ ExploreTours/Models/Tour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExploreTours.Models
 {
@@ -9,16 +10,20 @@ namespace ExploreTours.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [Display(Name = "Length in days")]
+        [Range(minimum: 1, maximum: 99)]
         public int Length { get; set; }
 
         public decimal Price { get; set; }
 
         public string Rating { get; set; }
 
+        [Display(Name = "Includes meals")]
         public bool IncludesMeals { get; set; }
     }
 }
